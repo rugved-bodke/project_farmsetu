62e1c02a74fc587e5e3fe507d790ecd3e2d64561
Rugved Bodke <rugvedbodke@yahoo.com>
2022-12-09 20:48:27 +0530

    fix: min temp not getting resp

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+python3.8 manage.py makemigrations hub
+python3.8 manage.py migrate hub
 python3.8 manage.py makemigrations
 python3.8 manage.py migrate
 python3.8 manage.py runserver 0:8000
